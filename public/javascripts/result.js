f83d4674475071ba8c48cb0e454a22740919dc46
Gabriel Guzman <guzman.gabriel@outlook.com>
2017-02-26 03:48:59 -0500

    Attempting to GET from Facebook

--- public/javascripts/result.js
+++ public/javascripts/result.js
@@ -1,116 +1,120 @@
 var userIdToSearch = "walmart"; //ID of the user who's profile we're searching
-		var users = [];
+var users = [];
 
-		function offense(offense)
-			{
-				this.offenses = [];
-				this.counter = 0;
+console.log('beginning');
 
-				this.addOffense = function(offense) {
-					offenses.push(offense); 
-					counter++;
-				}
-			}
-		function person(name,id,offense)
-			{
-				this.name = name;
-				this.id = id;
-				this.offense = new offense();
+function offense(offense)
+	{
+		this.offenses = [];
+		this.counter = 0;
 
-				this.getName = function() {
-					return name;
-				}
-
-				this.addOffense = function(text) {
-					this.offense.addOffense(text);
-				}
-
-				this.getOffensesCounter = function() {
-					return this.offense.counter;
-				}
-
-				this.getOffenses = function() {
-					return offenses;
-				}
-			}
-
-		function searchUsers(id){
-			var found = false;
-			users.forEach(checkId);
-
-			for (user in users) {
-				if(user.getName() === id) {
-					found = true;
-				}
-			}
-
-			return found;
+		this.addOffense = function(offense) {
+			offenses.push(offense); 
+			counter++;
 		}
-		
-		logInWithFacebook = function() { //
-			FB.login(function(response) {
-				if (response.authResponse) {
-					console('You are logged in & cookie set!');
-					var accessToken = response.authResponse.accessToken;
-					// Now you can redirect the user or do an AJAX request to
-					// a PHP script that grabs the signed request from the cookie
-					function testAPI() {
-						console.log('Welcome!  Fetching your information.... ');
+	}
+function person(name,id,offense)
+	{
+		this.name = name;
+		this.id = id;
+		this.offense = new offense();
 
-						FB.api(
-							'/' + userIdToSearch,
-							'GET',
-							{"fields":"posts{comments{created_time,from,message}}"},
-							function(response) {
-								//console.log(response);
-								posts = response['posts']['data'];
+		this.getName = function() {
+			return name;
+		}
 
-								var thisComment, authorId, authorName, commentBody;
+		this.addOffense = function(text) {
+			this.offense.addOffense(text);
+		}
 
-								for (i = 0; i < posts.length; i++) { //I'M USING INCONSISTENT LOOPS BECAUSE THEY'RE DUMB
+		this.getOffensesCounter = function() {
+			return this.offense.counter;
+		}
 
-									for (k = 0; k < posts[i]['comments']['data'].length; k++) {
+		this.getOffenses = function() {
+			return offenses;
+		}
+	}
 
-										//ask watson about individual comments
-										comment = posts[i]['comments']['data'][k];
-										authorId = comment['from']['id'];
-										authorName = comment['from']['name'];
+function searchUsers(id){
+	var found = false;
+	users.forEach(checkId);
 
-										commentBody = comment['message'];
+	for (user in users) {
+		if(user.getName() === id) {
+			found = true;
+		}
+	}
 
-										console.log(commentBody);
-										
-									}
-								}						  		
-						  }
-						);
-					}
+	return found;
+}
 
-					testAPI();
+logInWithFacebook = function() { //
+	FB.login(function(response) {
+		if (response.authResponse) {
+			console('You are logged in & cookie set!');
+			var accessToken = response.authResponse.accessToken;
+			// Now you can redirect the user or do an AJAX request to
+			// a PHP script that grabs the signed request from the cookie
+			function testAPI() {
+				console.log('Welcome!  Fetching your information.... ');
+
+				FB.api(
+					'/' + userIdToSearch,
+					'GET',
+					{"fields":"posts{comments{created_time,from,message}}"},
+					function(response) {
+						//console.log(response);
+						posts = response['posts']['data'];
+
+						var thisComment, authorId, authorName, commentBody;
+
+						for (i = 0; i < posts.length; i++) { //I'M USING INCONSISTENT LOOPS BECAUSE THEY'RE DUMB
+
+							for (k = 0; k < posts[i]['comments']['data'].length; k++) {
+
+								//ask watson about individual comments
+								comment = posts[i]['comments']['data'][k];
+								authorId = comment['from']['id'];
+								authorName = comment['from']['name'];
+
+								commentBody = comment['message'];
+
+								console.log(commentBody);
+								
+							}
+						}						  		
+				  }
+				);
+			}
+
+			testAPI();
 
 
-				} else {
-					alert('Login failed.');
-					console.log("FAILED");
-				}
-			});
-			return false;
-		};
+		} else {
+			alert('Login failed.');
+			console.log("FAILED");
+		}
+	});
+	return false;
+};
 
-		window.fbAsyncInit = function() {
-			FB.init({
-				appId: '165008204006667',
-				cookie: true, // This is important, it's not enabled by default
-				version: 'v2.8'
-			});
+window.fbAsyncInit = function() {
+	FB.init({
+		appId: '165008204006667',
+		cookie: true, // This is important, it's not enabled by default
+		version: 'v2.8'
+	});
 
 
-		};
+};
 
-		(function(d, s, id){
-			var js, fjs = d.getElementsByTagName(s)[0];
-			if (d.getElementById(id)) {return;}
-			js = d.createElement(s); js.id = id;
-			js.src = "https://connect.facebook.net/en_US/sdk.js";
-			fjs.parentNode.insertBefore(js, fjs);
-  		}(document, 'script', 'facebook-jssdk'));
+(function(d, s, id){
+	var js, fjs = d.getElementsByTagName(s)[0];
+	if (d.getElementById(id)) {return;}
+	js = d.createElement(s); js.id = id;
+	js.src = "https://connect.facebook.net/en_US/sdk.js";
+	fjs.parentNode.insertBefore(js, fjs);
+	}(document, 'script', 'facebook-jssdk'));
+
+console.log('end');
