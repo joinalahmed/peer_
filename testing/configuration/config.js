a5fec0888f43551ad3a3b195a9edc44e43e98ccd
Gabriel Guzman <guzman.gabriel@outlook.com>
2017-02-26 05:05:38 -0500

    holy fug

--- testing/configuration/config.js
+++
@@ -1,5 +0,0 @@
-module.exports={
-  "facebook_api_key"      :     "FB APP ID",
-  "facebook_api_secret"   :     "FB API SECRET",
-  "callback_url"          :     "http://localhost:3000/auth/facebook/callback"
-}
