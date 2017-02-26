c5f7be5c0ba760b031629a332c5d1d0ca93dc1a1
Fernando Quintero <ferdanielq@outlook.com>
2017-02-26 04:41:07 -0500

    Watson joined us :)

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 var routes = require('./routes');
+var watson = require('./watson');
 
 var app = express();
 
@@ -22,6 +23,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use('/watson', watson);
 app.use('/', routes);
 
 // catch 404 and forward to error handler
---
+++ watson.js
@@ -0,0 +1,26 @@
+﻿var express = require('express');
+var router = express.Router();
+var watson = require('watson-developer-cloud');
+
+var tone_analyzer = watson.tone_analyzer({
+    username: "d2e5af13-2ac6-4860-8066-ad1f77d2b252",
+    password: "EbHk45QRU0xv",
+    version: 'v3',
+    version_date: '2016-05-19 '
+});
+
+
+// url: peer-mlh.com/watson/search?text={the text}
+router.get('/search', function (req, res) {
+    console.log(req.query.text);
+
+    tone_analyzer.tone({ text: req.query.text },
+        function (err, tone) {
+            if (err)
+                console.log(err);
+            else
+                res.send(JSON.stringify(tone, null, 2));
+        });
+});
+
+module.exports = router;
