ac9aae401f36299c03d42efa40cc09cb3967c1d7
Gabriel Guzman <guzman.gabriel@outlook.com>
2017-02-27 15:35:23 -0500

    Testing some post/get requests

--- watson.js
+++ watson.js
@@ -1,7 +1,6 @@
 ﻿var express = require('express');
 var router = express.Router();
 var watson = require('watson-developer-cloud');
-var toSendBack;
 
 var tone_analyzer = watson.tone_analyzer({
     username: "d2e5af13-2ac6-4860-8066-ad1f77d2b252",
@@ -10,29 +9,25 @@ var tone_analyzer = watson.tone_analyzer({
     version_date: '2016-05-19 '
 });
 
-function callWatson(){
+// url: peer-mlh.com/watson/parse?id={the facebookId}
+// for testing url: peer-mlh.com/watson/parse?text=
+router.post('/parse', function(req,res){
+    /*
     tone_analyzer.tone({ text: "It's already December 23, 2016 and I have NOT received my shipment that was supposed to come the 22nd. I expect my $12 that I paid for expidited shipping back, since you obviously LIED about the arrival date! I checked the tracking and it says it'll come in the 27th, which is after Christmas and is RIDICULOUS because I PAID for EXPIDITED shipping and it did NOTHING. Now I have to tell my neice that I DON'T have a gift for her because WALMART LIED. Terrible company and terrible customer service. Target is 100x better than Walmart!" },
       function(err, tone) {
         if (err)
-          console.log(err);
-        else
-          console.log(JSON.stringify(tone, null, 2));
-    });
+            console.log(err);
+        else {
+            console.log(JSON.stringify(tone, null, 2));
 
-}
+        }
 
-router.get('/parse', function(req,res){
-    tone_analyzer.tone({ text: "It's already December 23, 2016 and I have NOT received my shipment that was supposed to come the 22nd. I expect my $12 that I paid for expidited shipping back, since you obviously LIED about the arrival date! I checked the tracking and it says it'll come in the 27th, which is after Christmas and is RIDICULOUS because I PAID for EXPIDITED shipping and it did NOTHING. Now I have to tell my neice that I DON'T have a gift for her because WALMART LIED. Terrible company and terrible customer service. Target is 100x better than Walmart!" },
-      function(err, tone) {
-        if (err)
-          console.log(err);
-        else
-          console.log(JSON.stringify(tone, null, 2));
-    });
+    }); */
+    res.send(req.body.text);
 })
 
 /*
-// url: peer-mlh.com/watson/parse?id={the facebookId}
+
 router.get('/parse', function (req, res) {
     tone_analyzer.tone({ text: "It's already December 23, 2016 and I have NOT received my shipment that was supposed to come the 22nd. I expect my $12 that I paid for expidited shipping back, since you obviously LIED about the arrival date! I checked the tracking and it says it'll come in the 27th, which is after Christmas and is RIDICULOUS because I PAID for EXPIDITED shipping and it did NOTHING. Now I have to tell my neice that I DON'T have a gift for her because WALMART LIED. Terrible company and terrible customer service. Target is 100x better than Walmart!" },
     function(err, tone) {
