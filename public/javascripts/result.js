5c76bfcc5ce57818a7ba38ba377d789f41361e36
Gabriel Guzman <guzman.gabriel@outlook.com>
2017-03-01 19:19:59 -0500

    Restructured code for localhost use

--- public/javascripts/result.js
+++ public/javascripts/result.js
@@ -145,5 +145,5 @@ window.fbAsyncInit = function() {
 	js = d.createElement(s); js.id = id;
 	js.src = "https://connect.facebook.net/en_US/sdk.js";
 	fjs.parentNode.insertBefore(js, fjs);
-	}(document, 'script', 'facebook-jssdk'));
+}(document, 'script', 'facebook-jssdk'));
 
