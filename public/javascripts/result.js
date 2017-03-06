24f2d44541a92fdab8cf8474af5605f644dbaaca
Gabriel Guzman <guzman.gabriel@outlook.com>
2017-03-06 02:21:29 -0500

    Maybe sorting people

--- public/javascripts/result.js
+++ public/javascripts/result.js
@@ -1,4 +1,4 @@
-var userIdToSearch = "walmart"; //ID of the user who's profile we're searching
+var userIdToSearch = "100009081525475"; //ID of the user who's profile we're searching
 src = "//ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js";
 var users = [];
 var offenses = [];
